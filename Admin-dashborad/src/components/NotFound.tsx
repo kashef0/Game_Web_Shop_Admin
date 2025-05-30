0dc201ba4a0e3dc1c4acd268964dd6f7131882db
M.kashef <kashef.sy@gmail.com>
2025-05-30 14:37:54 +0200

    la till notfound komonent och importerade till App

---
+++ Admin-dashborad/src/components/NotFound.tsx
@@ -0,0 +1,16 @@
+import { NavLink } from "react-router";
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
+    <img src="/public/404.png" alt="404 error" style={{width: '600px', height: '400px'}} />
+      <h1 className="text-5xl font-bold mb-4">Page not found</h1>
+      <p className="text-gray-600 mb-6">The page you’re looking for doesn’t exist or has been moved.</p>
+      <NavLink to="/Games_List" className="text-blue-500 hover:underline">
+        Go back to homepage
+      </NavLink>
+    </div>
+  );
+};
+
+export default NotFound;
